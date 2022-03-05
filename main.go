f822c63ac5f2820867d179d05281af31565f2dc1
grv.nikita <sitleman68@gmail.com>
2022-03-05 09:43:37 +0300

    TP-b0b add: db struct, get single note
    TP-b0b update: use interface for db, small fixes
    
    TP-b0b update: name fixes
    
    TP-b0b update architecture: router->interface->application->repository
    
    TP-b0b update: json tags, error check

--- main.go
+++ main.go
@@ -12,18 +12,24 @@ import (
 )
 
 func main() {
-	r := mux.NewRouter()
+	router := mux.NewRouter()
+
+	notesStorage := storage.NewNotesStorage()
+	notesService := application.NewNotesApp(notesStorage)
+	notesHandler := interfaces.NewNotesHandler(notesService)
+
+	routerAPI := router.PathPrefix("/api/v1").Subrouter()
+	routerAPI.HandleFunc("/note/{note-token:[0-9]+}", notesHandler.ReceiveSingleNote).Methods("GET")
 
 	securityManager := security.NewSimpleSecurityManager()
 	userStorage := storage.NewUserCacheStorage(securityManager)
 	userService := application.NewUserService(userStorage, securityManager)
-
 	register := interfaces.NewAuthHandler(userService)
 
-	r.HandleFunc("/users/signup", register.SignUp).Methods("POST")
+	router.HandleFunc("/users/signup", register.SignUp).Methods("POST")
 
 	fmt.Println("Start server at port 3000...")
-	err := http.ListenAndServe(":3000", r)
+	err := http.ListenAndServe(":3000", router)
 	if err != nil {
 		log.Fatal(err)
 	}
