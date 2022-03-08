a94b5843fa068fd2d6fe59db67738e32fdba8ce9
krulsaidme0w <mr.prilutskiy.200@mail.ru>
2022-03-08 19:14:15 +0300

    TP-2fa errors fixed

--- main.go
+++ main.go
@@ -15,24 +15,24 @@ func main() {
 	router := mux.NewRouter()
 
 	securityManager := security.NewSimpleSecurityManager()
-	
-  userStorage := storage.NewUserCacheStorage(securityManager)
-  notesStorage := storage.NewNotesStorage()
+
+	userStorage := storage.NewUserCacheStorage(securityManager)
+	notesStorage := storage.NewNotesStorage()
 	usersNotesStorage := storage.NewUsersNotesStorage(notesStorage)
-  sessionStorage := storage.NewSessionStorage()
-  
+	sessionStorage := storage.NewSessionStorage()
+
 	notesService := application.NewNotesApp(notesStorage, usersNotesStorage)
 	userService := application.NewUserService(userStorage, securityManager)
 	authService := application.NewAuthApp(sessionStorage, userService, securityManager)
-	
-  notesHandler := interfaces.NewNotesHandler(notesService)
+
+	notesHandler := interfaces.NewNotesHandler(notesService)
 	registerHandler := interfaces.NewAuthHandler(userService)
- 	loginHandler := interfaces.NewLoginHandler(authService)
+	loginHandler := interfaces.NewLoginHandler(authService)
 
 	routerAPI := router.PathPrefix("/api/v1").Subrouter()
 	routerAPI.HandleFunc("/note/{note-token:[0-9]+}", notesHandler.ReceiveSingleNote).Methods("GET")
 	routerAPI.HandleFunc("/notes", notesHandler.MainPage).Methods("GET")
-  routerAPI.HandleFunc("/users/login", loginHandler.Login).Methods("POST")
+	routerAPI.HandleFunc("/users/login", loginHandler.Login).Methods("POST")
 	routerAPI.HandleFunc("/users/logout", loginHandler.Logout).Methods("GET")
 	routerAPI.HandleFunc("/users/auth", loginHandler.Auth).Methods("GET")
 	routerAPI.HandleFunc("/users/signup", registerHandler.SignUp).Methods("POST")
